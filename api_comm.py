90dbc49e10f6784b1f1a4bdc9f19506c09af28b0
Florian Dellekart <florian.dellekart@hotmail.de>
2020-02-12 19:14:01 +0100

    getters and setters for origin, destination, only getter for request

--- api_comm.py
+++ api_comm.py
@@ -1,10 +1,37 @@
 
-#units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=YOUR_API_KEY
+#units=imperial&
 
-origin = {"latitude": "47.066502", "longitude": "15.438346"}
-destination = {"latitude": "47.063286", "longitude": "15.450824"}
-
-class APICommunicator:
+class DistanceMatrixCommunicator:
     def __init__(self, api_key_path):
-        self.api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-        self.api_key = open(api_key_path, 'r').read()
+        self._api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={}&destinations={}&key={}"
+        self._api_key = open(api_key_path, 'r').read()
+        self._origin = None
+        self._destination = None
+        self._request = None
+
+    @property
+    def origin(self):
+        return self._origin
+
+    @origin.setter
+    def origin(self, o):
+        self._origin = o
+
+    @property
+    def destination(self):
+        return self._destination
+
+    @destination.setter
+    def destination(self, d):
+        self._destination = d	
+
+    @property
+    def request(self):
+        if self._origin != None and self._destination != None:
+            self._request = self._api_url.format(self._origin, 
+                                                self._destination,
+                                                self._api_key
+                                                )
+            return self._request
+        else:
+            return self._request
--- test_api_comm.py
+++ test_api_comm.py
@@ -1,8 +1,20 @@
-from api_comm import APICommunicator
+from api_comm import DistanceMatrixCommunicator
 
-communicator = APICommunicator("google_api_key.txt")
+communicator = DistanceMatrixCommunicator("google_api_key.txt")
 
 def test_init():
-    assert isinstance(communicator.api_key, str)
-    assert communicator.api_key[-1] != "\n"
-    assert isinstance(communicator.api_url, str)
+    assert isinstance(communicator._api_key, str)
+    assert communicator._api_key[-1] != "\n"
+    assert isinstance(communicator._api_url, str)
+    assert communicator.origin == None
+    assert communicator.destination == None
+    assert communicator.request == None
+
+def test_getters_setters():
+    communicator.origin = "origin"
+    communicator.destination = "destination"
+
+    assert communicator.request == communicator._api_url.format("origin",
+                                                                "destination",
+                                                                communicator._api_key
+                                                                )
