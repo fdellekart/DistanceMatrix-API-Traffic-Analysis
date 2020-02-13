03c95a7187b123b6f01fab4034c1e08cc531be27
Florian Dellekart <florian.dellekart@hotmail.de>
2020-02-13 14:45:39 +0100

    added _get_timedelta_from_zero function and fixed lat/long bug in rqust

--- api_comm.py
+++ api_comm.py
@@ -1,14 +1,16 @@
 import requests
 import json
+from datetime import datetime
 
 
 class DistanceMatrixCommunicator:
     def __init__(self, api_key_path):
-        self._api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={}&destinations={}&key={}"
+        self._api_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={},{}&destinations={},{}&departure_time={}&key={}"
         self._api_key = open(api_key_path, 'r').read()
         self._origin = None
         self._destination = None
         self._request = None
+        self._departure_time = None
 
     @property
     def origin(self):
@@ -29,8 +31,11 @@ class DistanceMatrixCommunicator:
     @property
     def request(self):
         if self._origin != None and self._destination != None:
-            self._request = self._api_url.format(self._origin, 
-                                                self._destination,
+            self._request = self._api_url.format(self._origin['latitude'],
+                                                self._origin['longitude'],
+                                                self._destination['latitude'],
+                                                self._destination['longitude'],
+                                                str(self._get_timedalte_from_zero(self._departure_time))[:-2],
                                                 self._api_key
                                                 )
             return self._request
@@ -40,3 +45,8 @@ class DistanceMatrixCommunicator:
     def get_response(self):
         self.response = requests.get(self.request)
         return self.response
+
+    def _get_timedalte_from_zero(self, time_point):
+        time_zero = datetime(1970, 1, 1, 0, 0, 0)
+        time_delt = time_point - time_zero
+        return time_delt.total_seconds()
